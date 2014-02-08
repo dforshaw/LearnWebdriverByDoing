b63f108f5f0ab0d4e82c5892a927c169011ce26b
David Forshaw <dfcv126@gmail.com>
2014-02-07 17:05:49 -0800

    Chose a different page to use to demo FindElement By Name

--- src/test/java/com/learnwebdriverbydoing/inspection/FindElementByName.java
+++ src/test/java/com/learnwebdriverbydoing/inspection/FindElementByName.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import java.util.List;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -21,43 +19,12 @@ public class FindElementByName extends BasicWDSetupForInspection {
     @Test
     public void FindElementByNameTest() {
 
-        driver.get(baseUrl + "/" + "basic_html_form.html");
+        driver.get(baseUrl + "/" + "search.php");
 
-        // Determine how to locate to element on the page
-        By submitButtonLocator = By.name("submitbutton");
+        By searchButtonLocator = By.name("btnG");
+        WebElement searchButton = driver.findElement(searchButtonLocator);
+        assertThat(searchButton.getAttribute("value"), is("Search"));
 
-// Tried using a different field : Username
-//        By usernameLocator = By.name("username");
-//        WebElement usernameField = driver.findElement(usernameLocator);
-//        assertThat(usernameField.getSize(), is(equalTo(15)));
-
-        List<WebElement> buttons = driver.findElements(submitButtonLocator);
-
-//        WebElement submitButton = null;
-
-        for (WebElement button : buttons) {
-
-            String type =  button.getAttribute("type");
-// remove            System.out.println(type);
-
-            if (type.equals("submit")) {
-
-// ****** FIX THIS -- figure out how to set a WebElement element to another
-//                submitButton = button;
-                assertThat(button.getAttribute("value"), is("submit"));
-            }
-        }
-
-/*        try {
-        // Verify button text
-            assertThat(submitButton.getAttribute("value"), is("submit"));
-
-        } catch (NullPointerException e) {
-            System.out.println("getMessage - " + e.getMessage());
-            System.out.println("getStackTrace - " + e.getStackTrace());
-        }
-*/
         driver.close();
     }
-
 }
