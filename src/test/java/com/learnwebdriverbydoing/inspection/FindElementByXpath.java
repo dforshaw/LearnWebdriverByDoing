faa1970aca1fcc756f38ea3287bcb58aa95c33e9
David Forshaw <dfcv126@gmail.com>
2014-02-12 03:26:17 -0800

    Improved Xpath choice using Firepath plugin

--- src/test/java/com/learnwebdriverbydoing/inspection/FindElementByXpath.java
+++ src/test/java/com/learnwebdriverbydoing/inspection/FindElementByXpath.java
@@ -14,7 +14,10 @@ public class FindElementByXpath extends BasicWDSetupForInspection {
         driver.get(baseUrl + "/" + "basic_html_form.html");
 
         // Determine how to locate to element on the page
-        By submitButtonLocator = By.xpath("//input[@type='submit']");
+//        By submitButtonLocator = By.xpath("//input[@type='submit']");
+
+// NOTE: The above attempt worked but I chose improved choice below using FirePath
+        By submitButtonLocator = By.xpath(".//*[@id='HTMLFormElements']/table/tbody/tr[9]/td/input[2]");
 
         // Name the element on the page and hook it to the locator
         WebElement submitButton = driver.findElement(submitButtonLocator);
